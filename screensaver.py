5347eb8f8dedb4822e319a2439dd7e8fa57261c9
Abel <abel465@gmail.com>
2017-02-15 04:02:49 +0100

    Added error checking and correction for too many open files and too many symlink levels, restructured code to be easier to read

--- screensaver.py
+++ screensaver.py
@@ -1,111 +1,91 @@
-from collections import OrderedDict
 import tkinter as tk
-# from sortedcontainers import SortedListWithKey
 import argparse
 from sys import stderr
 import os
-from random import shuffle, randrange, randint
+from os import scandir
+from random import shuffle, randrange
 import PIL.ImageTk, PIL.ImageFile, PIL.Image
-# import bisect
+from itertools import chain
+
 
 PIL.ImageFile.LOAD_TRUNCATED_IMAGES = True
 
-IMAGE_TIME = 100
-BUFFER_LENGTH = 30
+IMAGE_TIME = 400
+BUFFER_SIZE = 1000
+DISPLAY_CHANCE_INVERSE = 100
+PRECACHE_DIR = 100
+TOO_MANY_OPEN_FILES = 24
+TOO_MANY_SYMLINK_LEVELS = 40
 
 
-#def next_ten(seq):
-#	for i in range(10):
-#		yield next(seq)
+def is_image(s):
+    return s.endswith(".png") or s.endswith(".jpg") or s.endswith(".svg")
+
 
 def random_directory_walk(dirs):
-	shuffle(dirs)
-	#iterators = [iter(d) for d in dirs]
-	while dirs:
-		directory = dirs.pop()
-		for f in os.scandir(directory):
-			if f.is_file():
-				if is_image(f.name):
-					yield directory + "/" + f.name
-			elif f.is_dir():
-				dirs.insert(randint(0, len(dirs)), directory + "/" + f.name)
+    file_buffer = []
+    shuffle(dirs)
+    iterators = [[d, None] for d in dirs]
 
-"""def random_os_walk(dirs):
-	if not dirs:
-		return
-	#print(dirs)
-	shuffle(dirs)
-	new_dirs = []
-	for directory in dirs:
-		for f in os.scandir(directory):
-#			print("f:", f.name, f.is_file, f.is_dir())
-			if f.is_file():
-				if is_image(f.name):
-					yield f.name
-			elif f.is_dir():  # is directory?
-				#print("dir:", f.name)
-				new_dirs.append(directory + "/" + f.name)
-	#print(new_dirs)
-	yield from random_os_walk(new_dirs)"""
-			
-			
-
-def bisect_right(a, x):
-    """Return the index where to insert item x in list a, assuming a is sorted.
-
-    The return value i is such that all e in a[:i] have e <= x, and all e in
-    a[i:] have e > x.  So if x already appears in the list, a.insert(x) will
-    insert just after the rightmost x already there.
-
-    MODIFIED FROM bisect.bisect_right SOURCECODE
-    """
-
-    lo = 0
-    hi = len(a)
-    while lo < hi:
-        mid = (lo+hi) // 2
-        if x < a[mid][0]:
-            hi = mid
-        else:
-            lo = mid + 1
-    return lo
-
-# def get_files(directory, indices, nums):
-#     n = 0
-#     # indices = [t[0] for t in v]
-#     for f in os.scandir(directory):
-#         if f.isfile and f.endswith(".png") or f.endswith(".jpg") or f.endswith(".svg"):
-#             if n in indices:
-#                 yield f.name, nums[indices.index(n)], directory
-#                 indices.remove(n)
-#                 if not indices:
-#                     return
-#             n += 1
-
-counter = {}
-
-def main(paths):
-    img = None
-    window = tk.Tk()
-    window.configure(background="black")
-    window.bind("<Key>", lambda e: e.widget.quit())
-    window.attributes("-fullscreen", True)
-
-    maxwidth, maxheight = window.winfo_screenwidth(), window.winfo_screenheight()
-
-    panel = tk.Label(window)
-    panel.pack()
-
-    directory_locations = []
-
-    def show_images(panel, seq):
-        nonlocal img
-
+    while iterators:
+        n = randrange(0, len(iterators))
+        directory, iterator = iterators[n]
+        if not iterator:
+            try:
+                while True:
+                    try:
+                        ez = scandir(directory)
+                    except OSError as e:
+                        raise e
+                        if e.errno == TOO_MANY_OPEN_FILES:
+                            for i in range(len(iterators)):
+                                if iterators[i][1]:
+                                    # evaluating next PRECACHE_DIR images to close files
+                                    evalutaion = tuple(next(iterators[i][1]) for _ in range(PRECACHE_DIR))
+                                    iterators[i][1] = chain(evaluation, iterators[i][1])
+                        else:
+                            raise e
+                    else:
+                        break
+                iterator = ez
+                iterators[n][1] = iterator
+            except PermissionError as e:
+                print("PermissionError", e, file=stderr)
+                del iterators[n]
+                continue
         try:
-            path = next(seq)
+            f = next(iterator)
         except StopIteration:
-            window.destroy()
-            return
+            del iterators[n]
+            continue
+        try:
+            is_file = f.is_file()
+        except OSError as e:
+            if e.errno != TOO_MANY_SYMLINK_LEVELS:
+                raise e
+        else:
+            if is_file:
+                if is_image(f.name):
+                    if not randrange(0, DISPLAY_CHANCE_INVERSE):
+                        yield f.path
+                    elif len(file_buffer) == BUFFER_SIZE:
+                        yield file_buffer.pop(randrange(0, len(file_buffer)))
+                    else:
+                        file_buffer.append(f.path)
+            elif f.is_dir() or f.is_symlink():
+                iterators.append([f.path, None])
+    shuffle(file_buffer)
+    yield from file_buffer
+
+
+def show_images(window, panel, seq, maxwidth, maxheight):
+    global img  # prevents image being garbage collected
+
+    try:
+        path = next(seq)
+    except StopIteration:
+        window.destroy()
+    else:
         print(path)
         try:
             im = PIL.Image.open(path)
@@ -116,120 +96,35 @@ def main(paths):
             panel.configure(image=img)
         except (OSError, IOError) as e:  # May not be real image, or IO problem
             print(e, file=stderr)
-            panel.after(0, show_images, panel, seq)
-            return
-        panel.after(IMAGE_TIME, show_images, panel, seq)
-
-    """
-    def get_paths(dirs):
-        used = []
-        total = 0
-        for f in random_directory_walk(dirs):
-            #if f.endswith(".png") or f.endswith(".jpg") or f.endswith(".svg"):
-            total += 1
-            if not randrange(0, 1000):
-                yield s
-        if total:
-            directory_locations.append([total, path])
-
-        directory_locations.sort(key=lambda pair: pair[0])
-        # print(directory_locations)
-        # print()
-        for i in range(1, len(directory_locations)):
-            directory_locations[i][0] += directory_locations[i-1][0]
-
-        # print()
-        # print(directory_locations)
-        # print(directory_locations[-1][0])
-        print("Finished counting number of media files:", directory_locations[-1][0])
-        nums = list(range(directory_locations[-1][0]))
-        shuffle(nums)
-        # print(nums)
-        # for x in [0, 1, 2 , 3, 4, 777, 762, 763, 764, 765, 5735]:
-        i = 0
-        j = BUFFER_LENGTH
-        length = len(nums)
-        while length > i:
-            # print(i, j)
-            d = OrderedDict()  # maps directories to indices of the directories
-            dd = OrderedDict()  # maps directories to indices of the whole structure
-            ez = nums[i:j]
-            # print("ez:", ez, len(nums))
-            # map_back = {}  # maps indices of the whole structure to directory and indices of the directories pairs
-            for x in ez:'
-                # x = 1#directory_locations[-1][0]-1
-                # print("x:", x)
-                a = bisect_right(directory_locations, x)
-                # print(a)
-                # try:
-                # print(directory_locations[a], end=" ")
-                if a == 0:
-                    index = x
-                    # print(x)
-                else:
-                    index = x-directory_locations[a-1][0]
-                    # print(x-directory_locations[a-1][0])
-                directory = directory_locations[a][1]
-                if directory in d:
-                    d[directory].append(index)
-                    dd[directory].append(x)
-                else:
-                    d[directory] = [index]
-                    dd[directory] = [x]
-                # map_back[x] = (directory, index)
+            panel.after(0, show_images, window, panel, seq, maxwidth, maxheight)
+        else:
+            panel.after(IMAGE_TIME, show_images, window, panel, seq, maxwidth, maxheight)
 
 
-            ll = []
-            dd_gen = iter(dd.items())
-            for k, v in d.items():  # directory, index
-                _, numss = next(dd_gen)
-                ll.extend(get_files(k, v, numss))
-            # print("ll:", ll)#[x[1] for x in ll])
-            ll.sort(key=lambda x:ez.index(x[1]))
-            yield from (x[2] + "/" + x[0] for x in ll)
-            # print("ll:", [x[1] for x in ll])
+def main(paths):
+    window = tk.Tk()
+    window.configure(background="black")
+    window.bind("<Key>", lambda e: e.widget.quit())
+    window.attributes("-fullscreen", True)
 
-            i = j
-            j += BUFFER_LENGTH
-        print(counter)
-    """
+    maxwidth, maxheight = window.winfo_screenwidth(), window.winfo_screenheight()
+
+    panel = tk.Label(window)
+    panel.pack()
 
     seq = random_directory_walk(paths)
-    show_images(panel, seq)
+    show_images(window, panel, seq, maxwidth, maxheight)
     window.mainloop()
 
-def is_image(s):
-    return s.endswith(".png") or s.endswith(".jpg") or s.endswith(".svg")
-
-def get_files(directory, indices, nums):
-    # print(indices, nums)
-    if directory in counter:
-        counter[directory] += 1
-    else:
-        counter[directory] = 1
-    n = 0
-    # indices = [t[0] for t in v]
-    for f in os.scandir(directory):
-        # help(f)
-        if f.is_file and is_image(f.name):
-            if n in indices:
-                # print(n, indices.index(n), indices)
-                yield f.name, nums[indices.index(n)], directory
-                # indices.remove(n)
-                if not indices:
-                    return
-            n += 1
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Screensaver program that works well with large quantity of media")
     parser.add_argument("image_time", help="The time that an image will stay on screen for")
     parser.add_argument("paths", nargs="+", help="A path for the screensaver to show media of")
     args = parser.parse_args()
-    # print(args.paths)
-    #exit
-    #print(args.paths)
-    #for x in random_os_walk(args.paths[:]):
-    #	print(x)
-    #print(args.paths)
-    #print(list(list(os.walk(p)) for p in args.paths))
+    # summ = 0
+    # for path, dirs, files in os.walk(args.paths[0]):
+    #     summ += sum(1 for f in files if is_image(f))
+    # print(summ)
+    # print(sum(1 for f in random_directory_walk(args.paths)))
     main(args.paths)
