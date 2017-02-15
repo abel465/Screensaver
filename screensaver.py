24c009b2cf00ad42efa8237684c61ed5391d2fde
Abel <abel465@gmail.com>
2017-02-15 06:50:39 +0100

    Program now continues if an expected image file or directory is no longer there

--- screensaver.py
+++ screensaver.py
@@ -1,9 +1,8 @@
-import tkinter as tk
-import argparse
+from tkinter import Tk, Label
+from argparse import ArgumentParser
 from sys import stderr
-import os
 from os import scandir
-from random import shuffle, randrange
+from random import Random, shuffle, randrange
 import PIL.ImageTk, PIL.ImageFile, PIL.Image
 from itertools import chain
 
@@ -17,6 +16,9 @@ PRECACHE_DIR = 100
 TOO_MANY_OPEN_FILES = 24
 TOO_MANY_SYMLINK_LEVELS = 40
 
+s = randrange(0, 2**63-1)
+r = Random(s)
+print(s)
 
 def is_image(s):
     return s.endswith(".png") or s.endswith(".jpg") or s.endswith(".svg")
@@ -24,34 +26,35 @@ def is_image(s):
 
 def random_directory_walk(dirs):
     file_buffer = []
-    shuffle(dirs)
+    r.shuffle(dirs)
     iterators = [[d, None] for d in dirs]
 
     while iterators:
-        n = randrange(0, len(iterators))
+        n = r.randrange(0, len(iterators))
         directory, iterator = iterators[n]
         if not iterator:
             try:
                 while True:
                     try:
-                        ez = scandir(directory)
+                        iterator = scandir(directory)
                     except OSError as e:
-                        raise e
                         if e.errno == TOO_MANY_OPEN_FILES:
                             for i in range(len(iterators)):
                                 if iterators[i][1]:
-                                    # evaluating next PRECACHE_DIR images to close files
-                                    evalutaion = tuple(next(iterators[i][1]) for _ in range(PRECACHE_DIR))
+                                    #  evaluating next PRECACHE_DIR images to close files
+                                    evaluation = tuple(next(iterators[i][1]) for _ in range(PRECACHE_DIR))
                                     iterators[i][1] = chain(evaluation, iterators[i][1])
                         else:
                             raise e
                     else:
                         break
-                iterator = ez
                 iterators[n][1] = iterator
             except PermissionError as e:
-                print("PermissionError", e, file=stderr)
+                print(e.__name__, e, file=stderr)
                 del iterators[n]
+                continue
+            except FileNotFoundError as e:
+                print(e.__name__, e, file=stderr)
                 continue
         try:
             f = next(iterator)
@@ -66,15 +69,15 @@ def random_directory_walk(dirs):
         else:
             if is_file:
                 if is_image(f.name):
-                    if not randrange(0, DISPLAY_CHANCE_INVERSE):
+                    if not r.randrange(0, DISPLAY_CHANCE_INVERSE):
                         yield f.path
                     elif len(file_buffer) == BUFFER_SIZE:
-                        yield file_buffer.pop(randrange(0, len(file_buffer)))
+                        yield file_buffer.pop(r.randrange(0, len(file_buffer)))
                     else:
                         file_buffer.append(f.path)
-            elif f.is_dir() or f.is_symlink():
+            elif f.is_dir():
                 iterators.append([f.path, None])
-    shuffle(file_buffer)
+    r.shuffle(file_buffer)
     yield from file_buffer
 
 
@@ -94,22 +97,22 @@ def show_images(window, panel, seq, maxwidth, maxheight):
             im = im.resize((int(ratio*w), int(ratio*h)), PIL.Image.ANTIALIAS)
             img = PIL.ImageTk.PhotoImage(image=im)
             panel.configure(image=img)
-        except (OSError, IOError) as e:  # May not be real image, or IO problem
-            print(e, file=stderr)
+        except (OSError, IOError, FileNotFoundError) as e:  # May not be real image, IO problem. or file no longer there
+            print(e.__name__, e, file=stderr)
             panel.after(0, show_images, window, panel, seq, maxwidth, maxheight)
         else:
             panel.after(IMAGE_TIME, show_images, window, panel, seq, maxwidth, maxheight)
 
 
 def main(paths):
-    window = tk.Tk()
+    window = Tk()
     window.configure(background="black")
     window.bind("<Key>", lambda e: e.widget.quit())
     window.attributes("-fullscreen", True)
 
     maxwidth, maxheight = window.winfo_screenwidth(), window.winfo_screenheight()
 
-    panel = tk.Label(window)
+    panel = Label(window)
     panel.pack()
 
     seq = random_directory_walk(paths)
@@ -118,8 +121,8 @@ def main(paths):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Screensaver program that works well with large quantity of media")
-    parser.add_argument("image_time", help="The time that an image will stay on screen for")
+    parser = ArgumentParser(description="Screensaver program that works well with large quantity of media")
+    parser.add_argument("-t", "--image_time", type=int, help="The time that each image will stay on screen for")
     parser.add_argument("paths", nargs="+", help="A path for the screensaver to show media of")
     args = parser.parse_args()
     # summ = 0
