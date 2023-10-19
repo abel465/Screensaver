fd6206098167f704ad1f88616eabc613ede17612
Abel Svoboda <abel465@gmail.com>
2023-10-19 14:41:52 +1300

    Increase wait time for autodisplay

--- screensaver_service.sh
+++ screensaver_service.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 scriptdir="$( dirname -- "$BASH_SOURCE"; )";
 root_geo=$(xwininfo -root | awk -F'[ +]' '$3 ~ /-geometry/ {print $4}')
 
-while sleep 6; do
+while sleep 60; do
     xwininfo -root -tree | grep $root_geo | grep -qv "\(Desktop\|has no name\)" \
-        || (( $(xprintidle) < 6000 )) \
+        || (( $(xprintidle) < 60000 )) \
         || $scriptdir/screensaver_from_config.py
 done
